c1b22e2cf1d5fdeafc89975da4639cc01f214a51
Tobi <adegbuji_tobi@yahoo.com>
2020-10-19 14:59:11 -0400

    Score Class Implemented

---
+++ src/Score.java
@@ -0,0 +1,31 @@
+public class Score {
+    private int score;
+
+    public Score(int score) {
+        this.score = score;
+    }
+
+    //Increments Score and prints it out
+    public void incrementScore(Board board){
+        if(ateApple(board)) score++;
+        System.out.println("Score: " + score);
+    }
+    //Checks if apple was eaten
+    public boolean ateApple(Board board){
+        for (String [] array: board.getBoard()) {
+            for (String element: array){
+                if(element.equals("@"))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
