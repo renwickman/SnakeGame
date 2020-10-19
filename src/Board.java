11adbc8b5cfd9b221efafcf3308183c8b6fa2eed
Anthony Renwick <renwickman@gmail.com>
2020-10-19 10:38:22 -0500

    Board & Game changes

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,16 @@
 public class Board {
+    private String[][] board;
+    private boolean isDead = false;
+    int appleCount = 0;
+
+    public Board(){
+        board = new String[20][20];
+        isDead = false;
+        appleCount = 0;
+    }
+
+    void boardGame(){
+        board = new String[20][20];
+    }
+
 }
--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class Game {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Board board = new Board();
+        boolean isDone = false;
+        do {
+            board.boardGame();
+            boolean isValid = false;
+            do {
+                System.out.println("Try Again?");
+                switch (in.next().toUpperCase()){
+                    case "Y":
+                        isValid = true;
+                        isDone = false;
+                        break;
+                    case "N":
+                        isValid = true;
+                        isDone = true;
+                        break;
+                    default:
+                        System.out.println("Not valid, but nice try");
+                        isValid = false;
+                }
+            } while(!isValid);
+        } while(!isDone);
+    }
+
 }
