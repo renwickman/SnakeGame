85da92b7743a96646b074d9c73ca21e36fa439ed
Herman Breckenridge <hbrecken@pnw.edu>
2020-10-19 11:27:35 -0500

    Herman commit

--- src/Board.java
+++ src/Board.java
@@ -1,7 +1,31 @@
 public class Board {
     private String[][] board;
     private boolean isDead = false;
-    int appleCount = 0;
+    private int appleCount = 0;
+
+    public String[][] getBoard() {
+        return board;
+    }
+
+    public void setBoard(String[][] board) {
+        this.board = board;
+    }
+
+    public boolean isDead() {
+        return isDead;
+    }
+
+    public void setDead(boolean dead) {
+        isDead = dead;
+    }
+
+    public int getAppleCount() {
+        return appleCount;
+    }
+
+    public void setAppleCount(int appleCount) {
+        this.appleCount = appleCount;
+    }
 
     public Board(){
         board = new String[20][20];
