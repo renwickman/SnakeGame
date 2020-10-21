ad8e958d14612e07069c8ed0be811a59b6bfa8e0
Tobi <adegbuji_tobi@yahoo.com>
2020-10-21 14:38:46 -0400

    Deleted score methods

--- src/Score.java
+++ src/Score.java
@@ -5,21 +5,6 @@ public class Score {
         this.score = score;
     }
 
-    //Increments Score and prints it out
-    public void incrementScore(Board board){
-        if(ateApple(board)) score++;
-        System.out.println("Score: " + score);
-    }
-    //Checks if apple was eaten
-    public boolean ateApple(Board board){
-        for (String[] array : board.getThisBoard()) {
-            for (String element: array){
-                if(element.equals("@"))
-                    return false;
-            }
-        }
-        return true;
-    }
 
     public int getScore() {
         return score;
